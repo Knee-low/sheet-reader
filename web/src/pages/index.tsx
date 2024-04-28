a0d8a0387412a958a6b5677f42420bfb86b43107
NiloJr <castillanonilo@gmail.com>
2024-04-28 16:01:21 +0800

    Added loading indicator

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Inter } from "next/font/google";
-import { Box, Link } from "@mui/material";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Box, CircularProgress, Link } from "@mui/material";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/spreadsheet-reader')
+  }, [router])
+
   return (
-    <Box sx={{ p: 20}}>
-      <Link href="/spreadsheet-reader">TO SPREADSHEET</Link>
+    <Box sx={{ px: '48%', py: '20%', height:'100vh' }}>
+      <CircularProgress disableShrink />
     </Box>
   );
 }
