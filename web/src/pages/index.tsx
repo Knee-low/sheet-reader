bcab5ec8a1d243bb34fc96abfe83465a97720cd8
NiloJr <castillanonilo@gmail.com>
2024-04-28 09:40:56 +0800

    Changed routes

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,39 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import * as React from "react";
+import { Box, Typography } from "@mui/material";
+import Link from "@mui/material/Link";
+import DataTable from "@/templates/DataGrid";
 
-export default function Home() {
-  const router = useRouter();
+export type SheetDataRow = [string, number, number, number, number];
 
-  useEffect(() => {
-    router.push('/spreadsheet-reader');
-  }, [router]);
+export default function Home({ sheetData }: { sheetData: SheetDataRow[] }) {
+  return (
+    <Box sx={{ color: "black", backgroundColor: "white", height: "100vh" }}>
+      <Box component="section" sx={{ p: 2 }}>
+        <DataTable rowsT={sheetData} />
+      </Box>
 
-  return null;
+      <Box sx={{ p: 5 }}>
+        <Typography>
+          <span>SAMPLE SHEET: </span>
+          <Link
+            href="https://docs.google.com/spreadsheets/d/1Qo8Mkw8zZR9azMDIRN_avz5pfPlTM3CBTcYuyADLHLg/edit?usp=sharing"
+            target="_blank"
+          >
+            https://docs.google.com/spreadsheets/d/1Qo8Mkw8zZR9azMDIRN_avz5pfPlTM3CBTcYuyADLHLg/edit?usp=sharing
+          </Link>
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export async function getServerSideProps() {
+  const req = await fetch("http://localhost:3000/api/hello");
+  const res = await req.json();
+
+  return {
+    props: {
+      sheetData: res.data,
+    },
+  };
 }
